c2424a4e5138ee41b2fff143a68ca24f30f93728
dannyBernier <46949659+dannyBernier@users.noreply.github.com>
2021-04-20 10:57:22 -0500

    fixed dependencies

--- SwashbucklersSwag/src/test/java/com/sk8/swashbucklers/integration/EmployeeIntegrationTest.java
+++ SwashbucklersSwag/src/test/java/com/sk8/swashbucklers/integration/EmployeeIntegrationTest.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
